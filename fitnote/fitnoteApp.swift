112c41ec064cb16b713e90b994111297710a259f
ben1san <saintpacorider@gmail.com>
2026-06-18 18:40:10 +0900

    first commit

---
+++ fitnote/fitnoteApp.swift
@@ -0,0 +1,32 @@
+//
+//  fitnoteApp.swift
+//  fitnote
+//
+//  Created by Hiroyuki Kubo on 2026/06/18.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct fitnoteApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
